3dd9502fed866a28821db4ea53a29c0cea1b8844
masum <56122183+masumrazait@users.noreply.github.com>
2024-11-11 23:49:33 +0530

    code changes

--- src/test/java/testCase/LaunchBrowser.java
+++ src/test/java/testCase/LaunchBrowser.java
@@ -1,17 +1,16 @@
 package testCase;
 
-import com.microsoft.playwright.Browser;
-import com.microsoft.playwright.BrowserType;
-import com.microsoft.playwright.Page;
-import com.microsoft.playwright.Playwright;
+import com.microsoft.playwright.*;
 
 public class LaunchBrowser {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
         Playwright playwright = Playwright.create();
         Browser browser = playwright.chromium().launch(new BrowserType.LaunchOptions().setHeadless(false));
-        Page page = browser.newPage();
+        BrowserContext browserContext = browser.newContext(new Browser.NewContextOptions().setViewportSize(1920, 1080));
+        Page page = browserContext.newPage();
         page.navigate("https://rahulshettyacademy.com/seleniumPractise/#/");
         System.out.println(page.title());
+        Thread.sleep(3000);
         page.close();
         playwright.close();
 
